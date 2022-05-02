0dd102ef50aebc9169265600081f8c160583b929
Suresh <sureshchouhan2009@gmail.com>
2022-05-03 02:22:17 +0530

    >ios

---
+++ NOC/NOC.Android/Implementation/MapBaseUrl_Android.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Android.Content.Res;
+using NOC.Droid.Implementation;
+using NOC.Interfaces;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(MapBaseUrl_Android))]
+namespace NOC.Droid.Implementation
+{
+    public class MapBaseUrl_Android : IBaseUrl
+    {
+        public string Get()
+        {
+            return "file:///android_asset/";
+
+            //string content;
+            //AssetManager assets = this.Assets;
+            //using (StreamReader sr = new StreamReader(assets.Open("read_asset.txt")))
+            //{
+            //    content = sr.ReadToEnd();
+            //}
+        }
+    }
+}
