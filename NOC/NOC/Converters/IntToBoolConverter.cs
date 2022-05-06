7b6f2baadc29f5793e5340076558cca66f3c59ac
Suresh <sureshchouhan2009@gmail.com>
2022-05-06 10:06:41 +0530

    >converter

---
+++ NOC/NOC/Converters/IntToBoolConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace NOC.Converters
+{
+    public class IntToBoolConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            try
+            {
+                var listCount = int.Parse(value.ToString());
+                if (listCount == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
