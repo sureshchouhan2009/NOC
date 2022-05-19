c22cf0a6ff7ac3fb735993268945ef1808bf44b1
Suresh <sureshchouhan2009@gmail.com>
2022-05-20 01:15:59 +0530

    Added Enum Folder

---
+++ NOC/NOC/Enums/UserTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace NOC.Enums
+{
+    public enum UserTypes
+    {
+        Applicant = 0,
+        //below three are known as processor
+        Reviewer = 1,
+        Officer = 3,
+        Stackholder = 4
+    }
+}
